8c6bf3171d30776b37e744a61fade6b15e9bfe6d
asif-963 <146640561+asif-963@users.noreply.github.com>
2023-10-15 19:51:56 +0530

    Update helpers.py

--- webpage/helpers.py
+++ webpage/helpers.py
@@ -9,9 +9,9 @@ from django.conf import settings
 def send_forget_password_mail(email, token):
     subject = 'Reset Password'
     reset_link =f'{settings.BASE_URL}/change_pass/{token}/'
-    message = f"You're receiving this email because you requested a password reset for your user account at Detecting Phishing Website .\n\n"
+    message = f"You're receiving this email because you requested a password reset for your user account at Suspicious Websites Detetcion .\n\n"
     message += f"Please go to the following page and choose a new password:\n{reset_link}\n\n"
-    message += "Thanks for using our site!\nThe Detecting Phishing Websites team"
+    message += "Thanks for using our site!\nThe  Suspicious Websites Detetcion team"
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject, message, email_from, recipient_list)
