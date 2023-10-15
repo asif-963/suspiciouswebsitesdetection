f32d2b2e8d7d844e485ff74c833c7dcd9a6e726d
Asif <muhammedasifs321@gmail.com>
2023-10-15 19:16:34 +0530

    Initial commit for Django project

--- webpage/admin.py
+++ webpage/admin.py
@@ -4,6 +4,9 @@ from .models import Message
 from .models import SuspiciousWebsite
 admin.site.register(SuspiciousWebsite)
 
+from .models import SqlinjectionWebsites
+admin.site.register(SqlinjectionWebsites)
+
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('user_name','user_email','user_message')
 
--- webpage/urls.py
+++ webpage/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from .import views
-from webpage.views import list_suspicious_websites 
+from webpage.views import list_suspicious_websites
+from webpage.views import list_sqlinjection_websites
 
 
 
@@ -15,11 +16,13 @@ urlpatterns = [
     path('about',views.about,name="about"),
     path('faq',views.faq,name='faq'),
     path('admin_user',views.admin_user,name='admin_user'),
-    
     path('detection',views.check_phishing,name='detection'),
+    path('sqli_detection',views.check_sqlinjection,name='sqli_detection'),
+
     path('admin_login',views.adminlogin,name='admin_login'),
     path('admin/', views.admin,name='admin'),
     path('blacklist',views.list_suspicious_websites,name='blacklist'),
+    path('sqli_list',views.list_sqlinjection_websites,name='sqli_list'),
     path('forget',views.forget,name='forget'),
     path('change_pass/<token>/',views.change_pass,name='change_pass'),
     
