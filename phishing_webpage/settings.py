20fb03a82e5ae5328f89eb787aef11d3cef774a4
asif-963 <146640561+asif-963@users.noreply.github.com>
2023-10-24 23:10:03 +0530

    Update settings.py

--- phishing_webpage/settings.py
+++ phishing_webpage/settings.py
@@ -138,9 +138,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_FROM = 'admn9630369@gmail.com'
-EMAIL_HOST_USER = 'admn9630369@gmail.com'
-EMAIL_HOST_PASSWORD = 'dwbnlwpmnunyviru'
+EMAIL_FROM = 'your@gmail.com'
+EMAIL_HOST_USER ='your@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-password'
 
 
 # Default primary key field type
